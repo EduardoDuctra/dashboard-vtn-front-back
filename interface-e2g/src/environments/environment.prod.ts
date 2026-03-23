b9fc0785cee0ec5b5b2eb2315f329197ba8b9ef0
Eduardo Ductra <eduardo.ductra@gmail.com>
2026-03-23 08:36:01 -0300

    front 23 marco

--- interface-e2g/src/environments/environment.prod.ts
+++ interface-e2g/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
 
   // apiUrl: 'https://api-eduardo.ceesp.ufsm.br/vtn',
-  apiUrl: 'http://localhost:5006/vtn',
+  apiUrl: 'http://200.18.45.228:5006/vtn',
+
+  // apiUrl: 'http://localhost:5006/vtn',
 };
